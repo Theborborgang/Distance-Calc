d929c5f520cbd1a0141056a1a66b362f5dc466c9
Theborborgang <jmbborbor@brc.pshs.edu.ph>
2026-08-25 08:51:41 +0800

    Improved.py

--- Improved.py
+++ Improved.py
@@ -1,8 +1,10 @@
 while True:
-    X = float(input("Please enter distance in kilometers: " ))
-    Y = 0.621371
-    Z = X*Y
-    print("The distance in miles is: ", Z)
+    KM = float(input("Please enter distance in kilometers: " ))
+    #this is the user input
+    Miles = 0.621371
+    Converted_Distance = KM*Miles
+     #We multiplied the given KM by how much 1km=mile
+    print("The distance in miles is: ", Converted_Distance)
     
     compute_again = input("Do you want to convert again? (yes/no): ").lower()   
     if compute_again != 'yes':
